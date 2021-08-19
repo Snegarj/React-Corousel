0bb5bd9763b5f4d5036b6d9fff7ccfdd415dcc7b
Snegarj <rjsnegaa@gmail.com>
2021-08-19 21:18:44 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -11,37 +11,48 @@ export default function App() {
 
     
         <div class="controls-top">
+            <div id="inline" style={{justifyContent:"space-between"}}>
+        <div class="float-left">
+            <div id="inline">
+                <div id="inline"  class="live">
+                    <span ></span>
+                    <p> LIVE </p>
+        
+                    </div>
+                           <h5> Upcoming Live Coaching</h5>
+                    </div>
+                    </div>
+
+
+
             <div class="float-right">
+               
                 <a class="btn-floating" href="#multi-item-example" data-slide="prev"><i class="fa fa-angle-left"
                           ></i></a>
                 <a class="btn-floating" href="#multi-item-example" data-slide="next">
                     <i class="fa fa-angle-right"   ></i></a>
-            </div>
+            </div></div>
         </div>
 
-        <ol class="carousel-indicators">
-            <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
-            <li data-target="#multi-item-example" data-slide-to="1"></li>
-
-        </ol>
-    
         <div class="carousel-inner" role="listbox">
 
             <div class="carousel-item active">
 
                 <div class="col-md-4" >
                     <div class="card mb-2">
-                        <img class="card-img-top" src="image.png" alt="Card image cap" />
-                        <div class="grad1" id="inline">
+                        <img class="card-img-top" src="https://stackblitz.com/files/react-uqquit/github/Snegarj/React-Corousel/master/src/image.png" alt="Card image cap" />
+                        <div class="grad2" id="inline">
                             <i class="material-icons" >access_time</i>
                             <p> 5 days left to enroll </p>
                         </div>
+                        <ul class="list-group">
+  <li class="list-group-item">  <h4 >UPSC IAS GS 2022 Fundation Live Coaching..</h4></li></ul>
                         <div class="card-body">
-                            <h4 class="card-title">IBPS PO & SBI PO Live Coaching in English Batch..</h4>
+                          
                             <p class="card-text">
                                 <div id="inline1">
                                     <i class="fa fa-caret-square-o-right" ></i>
-                                        <p style={{color:"gray"}}>240+ Live Classes</p>
+                                        <p style={{color:"gray"}}>3600+ Live Classes</p>
                                     
                                 </div>
                                 
@@ -61,28 +72,31 @@ export default function App() {
 
                 <div class="col-md-4" >
                     <div class="card mb-2">
-                        <img class="card-img-top" src="image.png" alt="Card image cap" />
-                        <div class="grad1" id="inline">
+                        <img class="card-img-top" src="https://stackblitz.com/files/react-uqquit/github/Snegarj/React-Corousel/master/src/image.png" alt="Card image cap" />
+                        <div class="grad2" id="inline">
                             <i class="material-icons" >access_time</i>
                             <p>Starts in 15 days</p>
 
 
                         </div>
+                        <ul class="list-group">
+  <li class="list-group-item">  <h4 >Civil Assistant Engineer Live Coaching Batch 2</h4></li></ul>
+                        
                         <div class="card-body">
-                            <h4 class="card-title">Civil Junior Engineer Live Coaching Batch 2</h4>
+                           
                             <p class="card-text">
                                 <div id="inline1">
                                     <i class="fa fa-caret-square-o-right" ></i>
-                                        <p style={{color:"gray"}}>240+ Live Classes</p>
+                                        <p style={{color:"gray"}}>230+ Live Classes</p>
                                     
                                 </div>  <div id="inline">
                                     <div id="inline1">
                                         <i class="material-icons" id="book"  >bookmark_border</i>
-                                        <p  style={{color:"gray"}}>9000+ Questions</p>
+                                        <p  style={{color:"gray"}}>5000+ Questions</p>
                                     
                                     </div><div id="inline1">
                                         <i class='far fa-sticky-note' ></i>
-                                        <p  style={{color:"gray"}}>120+ Notes</p></div></div>
+                                        <p  style={{color:"gray"}}>80+ Notes</p></div></div>
 
                             </p>
                         </div>
@@ -91,25 +105,27 @@ export default function App() {
 
                 <div class="col-md-4" >
                     <div class="card mb-2">
-                        <img class="card-img-top" src="image.png" alt="Card image cap" />
+                        <img class="card-img-top" src="https://stackblitz.com/files/react-uqquit/github/Snegarj/React-Corousel/master/src/image.png" alt="Card image cap" />
                         <div class="grad2" id="inline">
                             <i class="material-icons">access_time</i>
                             <p> Batch closes in 2 days </p></div>
+                            <ul class="list-group">
+  <li class="list-group-item">  <h4 class="card-title">BPSC Foundation Live Coaching(Pre + Main)</h4></li></ul>
                         <div class="card-body">
-                            <h4 class="card-title">UPSC CDS- 2 2021 Live Coaching Batch 3</h4>
+                           
                             <p class="card-text">
                                 <div id="inline1">
                                     <i class="fa fa-caret-square-o-right" ></i>
-                                    <p style={{color:"gray"}}>240+ Live Classes</p>
+                                    <p style={{color:"gray"}}>200+ Live Classes</p>
                                     
                                 </div>  <div id="inline">
                                     <div id="inline1">
                                         <i class="material-icons" id="book" >bookmark_border</i>
-                                        <p  style={{color:"gray"}}>9000+ Questions</p>
+                                        <p  style={{color:"gray"}}>2000+ Questions</p>
                                     
                                     </div><div id="inline1">
                                         <i class='far fa-sticky-note' ></i>
-                                        <p  style={{color:"gray"}}>120+ Notes</p></div></div>
+                                        <p  style={{color:"gray"}}>300+ Notes</p></div></div>
                             </p>
                         </div>
                     </div>
@@ -122,12 +138,15 @@ export default function App() {
 
                 <div class="col-md-4" >
                     <div class="card mb-2">
-                        <img class="card-img-top" src="image.png" alt="Card image cap"  />
+                        <img class="card-img-top" src="https://stackblitz.com/files/react-uqquit/github/Snegarj/React-Corousel/master/src/image.png" alt="Card image cap"  />
                         <div class="grad1" id="inline">
                             <i class="material-icons" >access_time</i>
                             <p> 5 days left to enroll </p></div>
+
+                            <ul class="list-group">
+  <li class="list-group-item"><h4 class="card-title">IBPS PO & SBI PO Live Coaching in English Batch..</h4></li></ul>
                         <div class="card-body">
-                            <h4 class="card-title">IBPS PO & SBI PO Live Coaching in English Batch..</h4>
+                            
                             <p class="card-text">
                             <div id="inline1">
                                 <i class="fa fa-caret-square-o-right" ></i>
@@ -150,25 +169,27 @@ export default function App() {
 
                 <div class="col-md-4" >
                     <div class="card mb-2">
-                        <img class="card-img-top" src="image.png" alt="Card image cap"   />  
+                        <img class="card-img-top" src="https://stackblitz.com/files/react-uqquit/github/Snegarj/React-Corousel/master/src/image.png" alt="Card image cap"   />  
                         <div class="grad1" id="inline">
                             <i class="material-icons" >access_time</i>
                             <p>Starts in 15 days</p> </div>
+                            <ul class="list-group">
+  <li class="list-group-item"><h4 class="card-title">Civil Junior Engineer Live Coaching Batch 2</h4> </li></ul>
                         <div class="card-body">
-                            <h4 class="card-title">Civil Junior Engineer Live Coaching Batch 2</h4>
+                            
                             <p class="card-text">
                              <div id="inline1">
                                 <i class="fa fa-caret-square-o-right" ></i>
-                                <p style={{color:"gray"}}>240+ Live Classes</p>
+                                <p style={{color:"gray"}}>220+ Live Classes</p>
                                 
                             </div>  <div id="inline">
                                 <div id="inline1">
                                     <i class="material-icons" id="book"  >bookmark_border</i>
-                                    <p  style={{color:"gray"}}>9000+ Questions</p>
+                                    <p  style={{color:"gray"}}>5000+ Questions</p>
                                 
                                 </div><div id="inline1">
                                     <i class='far fa-sticky-note' ></i>
-                                    <p  style={{color:"gray"}}>120+ Notes</p></div></div>
+                                    <p  style={{color:"gray"}}>80+ Notes</p></div></div>
 
                             </p>
                         </div>
@@ -177,14 +198,16 @@ export default function App() {
 
                 <div class="col-md-4" >
                     <div class="card mb-2">
-                        <img class="card-img-top" src="image.png" alt="Card image cap"   />
+                        <img class="card-img-top" src="https://stackblitz.com/files/react-uqquit/github/Snegarj/React-Corousel/master/src/image.png" alt="Card image cap"   />
                         <div class="grad2" id="inline">
                             <i class="material-icons" >access_time</i>
                             <p> Batch closes in 2 days </p>
 
                         </div>
+                        <ul class="list-group">
+  <li class="list-group-item">   <h4 class="card-title">UPSC CDS- 2 2021 Live Coaching Batch 3</h4> </li></ul>
                         <div class="card-body">
-                            <h4 class="card-title">UPSC CDS- 2 2021 Live Coaching Batch 3</h4>
+                         
                             <p class="card-text">
                                  <div id="inline1">
                                     <i class="fa fa-caret-square-o-right" ></i>
@@ -194,11 +217,11 @@ export default function App() {
                             <div id="inline">
                                 <div id="inline1">
                                     <i class="material-icons" id="book"   >bookmark_border</i>
-                                    <p  style={{color:"gray"}}>9000+ Questions</p>
+                                    <p  style={{color:"gray"}}>6000+ Questions</p>
                                 
                                 </div><div id="inline1">
                                     <i class='far fa-sticky-note' ></i>
-                                    <p  style={{color:"gray"}}>120+ Notes</p></div></div>
+                                    <p  style={{color:"gray"}}>100+ Notes</p></div></div>
                 
                             </p>
                         </div>
